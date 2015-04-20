871b1cc0fdcfb62f326360c03c47f45621d0f4db
Andreas Copan <avcopan@gmail.com>
2015-04-20 17:12:50 -0400

    more

--- theory/odc12.py
+++ theory/odc12.py
@@ -75,12 +75,12 @@ class SpinOrbODC12:
         Ep2 = spinorb.build_Ep2(fockmat = tF)
 
         R   = indx.meinsums('ijab',
-              [ 1.  , P("ab")   , (tF,"ac"  ), (L,"ijcb")],
-              [-1.  , P("ij")   , (tF,"ki"  ), (L,"kjab")],
-              [ 1.  , I         , (g ,"ijab")            ],
-              [ 1./2, I         , (g ,"ijkl"), (L,"klab")],
-              [ 1./2, I         , (g ,"abcd"), (L,"ijcd")],
-              [-1.  , P("ij|ab"), (g ,"kbjc"), (L,"ikac")] )
+                [ 1.  , P("ab")   , (tF,"ac"  ), (L,"ijcb")],
+                [-1.  , P("ij")   , (tF,"ki"  ), (L,"kjab")],
+                [ 1.  , I         , (g ,"ijab")            ],
+                [ 1./2, I         , (g ,"ijkl"), (L,"klab")],
+                [ 1./2, I         , (g ,"abcd"), (L,"ijcd")],
+                [-1.  , P("ij|ab"), (g ,"kbjc"), (L,"ikac")] )
 
         L   = L + Ep2 * R
 
