bb31842d9cc6c04fa91cab58f8b728d38b30333c
Andreas V Copan <avcopan@gmail.com>
2015-04-22 23:08:18 -0400

    now optimizing the order of contraction

--- lib/index.py
+++ lib/index.py
@@ -1,4 +1,5 @@
 import numpy     as np
+import itertools as it
 import lib.block as bl
 
 class Index:
@@ -19,6 +20,16 @@ class Index:
     def trim  (self, array, index): return bl.trim  (array, self.ranges(index))
     def extend(self, array, index): return bl.extend(array, self.ranges(index))
 
+    def get_cost(self, *arrayindexpairs):
+      arrays, indices = zip(*arrayindexpairs)
+      cost = sum( np.product([stop-start for start, stop in self.ranges(index)])
+                  for index in accumulate_indices_pairwise(indices) )
+      return cost
+
+    def optimize_order(self, *arrayindexpairs):
+      reorderedpairs = min(it.permutations(arrayindexpairs), key=lambda x:self.get_cost(*x))
+      return [(self.trim(array, index).block, index) for array, index in reorderedpairs]
+
     def einsum(self, targetindex, *arrayindexpairs):
       arrayarg     = tuple(self.trim(array, index).block for array, index in arrayindexpairs)
       indexarg     = ','.join(index for array, index in arrayindexpairs) + '->' + targetindex
@@ -28,7 +39,7 @@ class Index:
       return bl.ArrayBlock(block, ranges, arrayshape).view()
 
     def eindot(self, targetindex, *arrayindexpairs):
-      pairs        = [(self.trim(array, index).block, index) for array, index in arrayindexpairs]
+      pairs        = self.optimize_order(*arrayindexpairs)
       array, index = pairs.pop(0)
       for pair in pairs: array, index = tensordot((array, index), pair)
       block        = transpose(array, targetindex, index)
@@ -51,12 +62,26 @@ def tensordot((array1, index1), (array2, index2)):
   axes1 = [index1.index(char) for char in index1 if char in index2]
   axes2 = [index2.index(char) for char in index1 if char in index2]
   array = np.tensordot(array1, array2, (axes1, axes2))
-  index = ''.join(char for char in index1+index2 if not (char in index1 and char in index2))
+  index = get_uncontracted_indices(index1, index2)
   return array, index
 
 def transpose(array, targetindex, index):
   axes = tuple(index.index(char) for char in targetindex)
   return array.transpose(*axes)
+
+def accumulate_indices_pairwise(indextuple):
+  indexlist = list(indextuple)
+  index     = indexlist.pop(0)
+  for nextindex in indexlist:
+    contracted = get_contracted_indices  (index, nextindex)
+    index      = get_uncontracted_indices(index, nextindex)
+    yield index+contracted
+
+def get_uncontracted_indices(index1, index2):
+  return ''.join(char for char in     index1+index2  if not (char in index1 and char in index2))
+
+def get_contracted_indices(index1, index2):
+  return ''.join(char for char in set(index1+index2) if     (char in index1 and char in index2))
 
 def check_args(indx, start, stop, chars):
   if not 0 <= start <= stop <= indx.dim:
